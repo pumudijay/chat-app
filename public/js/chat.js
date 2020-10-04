9ae3004bbdc8023d1dd2e4436a7b50cb5a9b7c41
pumudijay <jayasinghepumudi08@gmail.com>
2020-10-04 21:15:24 +0530

    event handle

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,6 +10,14 @@ document.querySelector('#increment').addEventListener('click', () => {
 })
 */
 
-socket.on('welcomeMsg', (message) => {
+socket.on('message', (message) => {
     console.log(message)
+})
+
+document.querySelector('#messageForm').addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const message = e.target.elements.message.value
+    //const message = document.querySelector('input').value
+    socket.emit('sendMessage',message)
 })
--- src/index.js
+++ src/index.js
@@ -36,14 +36,12 @@ io.on('connection', (socket) => {
 io.on('connection', (socket) => {
     console.log('New Websocket connection')
 
-    socket.emit('welcomeMsg','WELCOME!')
+    socket.emit('message','WELCOME!')
 
-   /* socket.on('increment', () => {
-
-        count++
-        //socket.emit('countUpdated',count)
-        io.emit('countUpdated', count)
-    })*/
+    socket.on('sendMessage', (message) => {
+        io.emit('message', message)
+        
+    })
 })
 
 server.listen(port, () =>{
