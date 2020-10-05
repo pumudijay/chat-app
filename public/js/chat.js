ac16c167894f35e7439ff2672c4844d9790a8b1e
pumudijay <jayasinghepumudi08@gmail.com>
2020-10-05 17:21:26 +0530

    rendering messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,21 +1,51 @@
 const socket = io()
 
+//Elements
+const $messageForm = document.querySelector('#messageForm')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#sendLocation')
+const $messages = document.querySelector('#messages')
+
+//Templates
+const messageTemplate = document.querySelector('#messageTemplate').innerHTML
+const locationMessageTemplate = document.querySelector('#locationMessageTemplate').innerHTML
+
 socket.on('message', (message) => {
     console.log(message)
+    const html = Mustache.render(messageTemplate,{
+        message
+    })
+    $messages.insertAdjacentHTML('beforeend',html)
 })
 
-document.querySelector('#messageForm').addEventListener('submit', (e) =>{
+socket.on('locationMessage', (url) => {
+    console.log(url)
+    const html=Mustache.render(locationMessageTemplate,{
+        url
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+$messageForm.addEventListener('submit', (e) =>{
     e.preventDefault()
+
+    //$messageFormButton.setAttribute('disabled', 'disabled')
 
     const message = e.target.elements.message.value
     //const message = document.querySelector('input').value
-    socket.emit('sendMessage',message,(error) => {
+
+    socket.emit('sendMessage', message)
+    /*socket.emit('sendMessage', message, (error) => {
+        $messageFormButton.removeAttribute('disabled')
+        $messageFormInput.value = ''
+        $messageFormInput.focus()
         if(error){
             return console.log(error)
-        }
-
+        }    
         console.log('Message delivered')
-    })
+        
+    })*/
 })
 
 document.querySelector('#sendLocation').addEventListener('click', () => {
